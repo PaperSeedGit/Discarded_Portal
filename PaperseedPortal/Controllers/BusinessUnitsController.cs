ff5672cf592b9da5f2d94286532aedc869380010
Krishna Mohan <kris.paperseed@gmail.com>
2015-02-10 20:03:58 +0530

    Added new project

---
+++ PaperseedPortal/Controllers/BusinessUnitsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaperseedPortal.Controllers
+{
+    public class BusinessUnitsController : Controller
+    {
+        //
+        // GET: /BusinessUnits/
+
+        public ActionResult Retail()
+        {
+            return View();
+        }
+
+    }
+}
